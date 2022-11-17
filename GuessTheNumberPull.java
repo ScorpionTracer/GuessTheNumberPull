2c2c3ee534e73dacd3bab719a7fe559c242ca754
ScorpionTracer <badik-9494@mail.ru>
2022-11-17 15:27:48 +0500

    Format

--- GuessTheNumberPull.java
+++ GuessTheNumberPull.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 
-    public class GuessTheNumber {
+public class GuessTheNumber {
 
-        public static void main(String[] args) {
-            var scanner = new Scanner(System.in);
-            System.out.println("Ваша задача удагать число");
-            int range = 10;
-            int number = (int) (Math.random() * range);
-            while (true) {
-                System.out.println("Угадайте число от 0 до " + range);
-                int InputNumber = scanner.nextInt();
-                if (InputNumber == number) {
-                    System.out.println("Вы угадали число");
-                    break;
-                } else if (InputNumber > number) {
-                    System.out.println("Загаданное число меньше");
-                } else {
-                    System.out.println("Загаданное число больше");
-                }
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("Ваша задача удагать число");
+        int range = 10;
+        int number = (int) (Math.random() * range);
+        while (true) {
+            System.out.println("Угадайте число от 0 до " + range);
+            int InputNumber = scanner.nextInt();
+            if (InputNumber == number) {
+                System.out.println("Вы угадали число");
+                break;
+            } else if (InputNumber > number) {
+                System.out.println("Загаданное число меньше");
+            } else {
+                System.out.println("Загаданное число больше");
             }
-            scanner.close();
         }
+        scanner.close();
     }
+}
